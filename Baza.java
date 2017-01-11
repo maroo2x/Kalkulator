757aee525f61f032d25bc34e79b9fd2f2587a3c3
maroo2x <kurdzie@gmail.com>
2017-01-11 16:24:55 +0100

    Update Baza.java

--- Baza.java
+++ Baza.java
@@ -9,6 +9,7 @@ import java.text.DateFormat;
 //import java.util.LinkedList;
 //import java.util.List;
 import java.util.Date;
+import java.util.Scanner;
 //import java.text.DateFormat;
 import java.text.SimpleDateFormat;
  
@@ -37,6 +38,28 @@ public class Baza {
         }
         createTables();
     }
+    
+    public boolean update(int id)  {		//data update
+    	try {
+    	Scanner scan = new Scanner(System.in);
+        ResultSet select = stat.executeQuery("SELECT * from POJAZD where id = "+id);
+        int lastPrze = select.getInt("przebieg");
+    	System.out.println("Podaj wartosc przebiegu: (poprzednia wartosc "+lastPrze+")");
+    	String wartprzeb = scan.nextLine();
+        float lastTank = select.getFloat("tankowanie");
+    	System.out.println("Podaj ilosc paliwa: (poprzednia wartosc "+lastTank+")");
+    	String tank = scan.nextLine();
+        String update = ("UPDATE pojazd set przebieg = "+wartprzeb+", tankowanie = "+tank+" where id = "+id);
+        stat.execute(update);
+        } catch (SQLException e) {
+            System.err.println("Blad przy update");
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+
 
     public boolean createTables()  {		//create table if not exists
         String createPojazd = "CREATE TABLE IF NOT EXISTS pojazd (id INTEGER PRIMARY KEY AUTOINCREMENT, przebieg int, tankowanie float, spalanie float, full boolean, rezerwa boolean, stacja varchar(255), data date)";
@@ -49,16 +72,19 @@ public class Baza {
         }
         return true;
     }
-    
-    
+
     public boolean insert(int przebieg, float tankowanie) {
         try {
-       	DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+        	DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
         	Date date = new Date();
             ResultSet select = stat.executeQuery("SELECT * from POJAZD order by id desc limit 1");
             float spalanie = 0;
             if (select.isBeforeFirst() == true){	//if first loading, do not count
-        	spalanie = (tankowanie/(przebieg-select.getFloat("przebieg"))*100);}
+                int lastPrze = select.getInt("przebieg");
+                if (lastPrze >= przebieg){ 
+            		System.out.println("Wprowadz przebieg wiekszy od poprzedniego");
+            		return false;}
+        	spalanie = (tankowanie/(przebieg-lastPrze)*100);}
             PreparedStatement prepStmt = conn.prepareStatement(		//insert values
                     "insert into pojazd values (NULL, ?, ?, ?, NULL, NULL, NULL, ?);"); //id, przebieg, tankowanie, spalanie, full, rezerwa, stacja, data
             prepStmt.setInt(1, przebieg);
